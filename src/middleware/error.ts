50a64fb2f93a6a3eff3737ca22ad6a1075ecc5bd
emiperezdev <emiperez.dev@gmail.com>
2024-08-26 19:47:51 -0600

    Add dist folder on .gitignore file

--- src/middleware/error.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-const error = (error: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log(error, error.message);
-  res.status(500).send('SOMETHING FAILED ://');
-}
-
-export default error;
